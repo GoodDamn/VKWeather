b4af642f9f07ef002c776216c6d12ffcdaac3d2a
GoodDamn <grigorydum@gmail.com>
2024-03-25 05:53:00 -0700

    UILabelImage

---
+++ VKWeather/other/views/UILabelImage.swift
@@ -0,0 +1,76 @@
+//
+//  UILabelImage.swift
+//  VKWeather
+//
+//  Created by GoodDamn on 25/03/2024.
+//
+
+import UIKit
+
+final class UILabelImage
+    : UILabel {
+    
+    private final let mImageAttachment:
+        NSTextAttachment
+    
+    public var leftImage: UIImage? {
+        didSet {
+            mImageAttachment.image = leftImage
+        }
+    }
+    
+    override init(frame: CGRect) {
+        mImageAttachment = NSTextAttachment()
+        super.init(
+            frame: frame
+        )
+    }
+    
+    required init?(
+        coder: NSCoder
+    ) {
+        mImageAttachment = NSTextAttachment()
+        super.init(
+            coder: coder
+        )
+    }
+    
+    public final func render() {
+        
+        guard let text = text else {
+            return
+        }
+        
+        let attrMain = NSMutableAttributedString(
+            string: " \(text)"
+        )
+        
+        let attrImage = NSMutableAttributedString(
+            attachment: mImageAttachment
+        )
+        
+        let textRange = NSRange(
+            location: 0,
+            length: attrMain.length
+        )
+        
+        attrMain.addAttribute(
+            .foregroundColor,
+            value: textColor,
+            range: textRange
+        )
+        
+        attrMain.addAttribute(
+            .font,
+            value: font,
+            range: textRange
+        )
+        
+        attrImage.append(
+            attrMain
+        )
+        
+        attributedText = attrImage
+    }
+    
+}
