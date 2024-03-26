bebbb7450bd20ba858f14add71a963ee7e6fb384
GoodDamn <grigorydum@gmail.com>
2024-03-26 08:42:50 -0700

    Loading res-icons on launch stage

--- VKWeather/AppDelegate.swift
+++ VKWeather/AppDelegate.swift
@@ -10,10 +10,52 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    public static var mIconsMap =
+        [String: UIImage?]()
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions
+        launchOptions: [
+            UIApplication.LaunchOptionsKey: Any
+        ]?
+    ) -> Bool {
+        
+        loadImage(
+            id: "01",
+            image: "sun.max.fill"
+        )
+        
+        loadImage(
+            id: "02",
+            image: "cloud.sun.fill"
+        )
+        
+        loadImage(
+            id: "03",
+            image: "cloud.fill"
+        )
+        
+        loadImage(
+            id: "04",
+            image: "cloud.fill"
+        )
+        
+        loadImage(
+            id: "10",
+            image: "cloud.rain.fill"
+        )
+        
+        loadImage(
+            id: "11",
+            image: "cloud.bolt.rain.fill"
+        )
+        
+        loadImage(
+            id: "50",
+            image: "cloud.fog.fill"
+        )
+        
         return true
     }
 
@@ -31,6 +73,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    private func loadImage(
+        id: String,
+        image: String
+    ) {
+        AppDelegate.mIconsMap[
+            id
+        ] = UIImage(
+            systemName: image
+        )
+    }
 
 }
 
