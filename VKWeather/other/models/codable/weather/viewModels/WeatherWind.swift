fc4b78fe7bdee4635beb686e12562457a721d5b8
GoodDamn <grigorydum@gmail.com>
2024-03-28 04:15:10 -0700

    Wind struc

---
+++ VKWeather/other/models/codable/weather/viewModels/WeatherWind.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherWind.swift
+//  VKWeather
+//
+//  Created by GoodDamn on 28/03/2024.
+//
+
+import Foundation
+public struct WeatherWind
+    : Codable {
+    let speed: Float
+    let deg: Int
+    let gust: Float
+}
